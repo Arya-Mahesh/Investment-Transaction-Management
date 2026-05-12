14f1029a4036d1cccb690934f3190c067774cb13
Arya-Mahesh <aryamahesh769@gmail.com>
2026-05-12 19:13:00 +0530

    Updated environment variables

--- src/main/java/com/rinar/trasactional_mgmt/config/DataSeeder.java
+++ src/main/java/com/rinar/trasactional_mgmt/config/DataSeeder.java
@@ -2,6 +2,7 @@ package com.rinar.trasactional_mgmt.config;
 
 import com.rinar.trasactional_mgmt.model.User;
 import com.rinar.trasactional_mgmt.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +10,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DataSeeder implements ApplicationRunner {
+
+    @Value("${ADMIN_PASSWORD}")
+    private String adminPassword;
+
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -22,7 +27,7 @@ public class DataSeeder implements ApplicationRunner {
         if (userRepository.findByUsername("admin").isEmpty()) {
             User admin = new User();
             admin.setUsername("admin");
-            admin.setPassword(passwordEncoder.encode("admin123"));
+            admin.setPassword(passwordEncoder.encode(adminPassword));
             admin.setRole("ADMIN");
             userRepository.save(admin);
             System.out.println("Default admin created");
